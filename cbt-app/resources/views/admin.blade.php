8fec6dbb3064cc97fa394b87d22e92c8d6cc5e38
Mr.Dominic <ochu.dominic.onah@gmail.com>
2025-04-03 17:59:23 +0100

    Navbar setup done

--- cbt-app/resources/views/admin.blade.php
+++ cbt-app/resources/views/admin.blade.php
@@ -14,7 +14,13 @@
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 
-<body>
+<body class="sb-nav-fixed">
+
+
+
+
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
