6b5c9aa1d61979b4624d9098e0552ab61e616470
Mr.Dominic <ochu.dominic.onah@gmail.com>
2025-05-10 23:11:50 +0100

    created all the session file/blade

---
+++ cbt-app/resources/views/admin/sessions/create.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.admin-dashboard')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                {{-- SUCCESS MESSAGE --}}
+                @if(session('status'))
+                    <h5 class="alert alert-success">{{ session('status') }} </h5>
+                @endif
+
+                @if(session('error'))
+                    <h5 class="alert alert-danger">
+                        {{ session('error') }}
+                    </h5>
+                @endif
+               
+
+                {{-- END OF SESSION MESSAGE --}}
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Create Academic Session <a class="btn btn-danger float-end" href="{{ route('sessions.index') }}">Back to List</a>
+                        </h4>
+
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('sessions.store') }}" method="POST" >
+                            @csrf
+                            <div class="form-group my-3">
+                                <label for="">Session Name (e.g., 2024/2025)</label>
+                                <input type="text" class="form-control" name="name" required>
+                                @error('name')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="form-group my-3 form-check">
+                                <input type="checkbox" name="is_active" class="form-check-input" id="is_active"> 
+                                <label for="is_active">Set as Active</label>
+                            </div>
+                            <div class="form-group my-3">
+                                <button type="submit" class="btn btn-primary">Save Session</button>
+                                <a href="{{route('sessions.index')}}" class="btn btn-secondary">Cancel</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
