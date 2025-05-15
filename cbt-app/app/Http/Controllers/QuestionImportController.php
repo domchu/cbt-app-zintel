065c81d33df5ab03a67680ff915ce726d40a29a9
Mr.Dominic <ochu.dominic.onah@gmail.com>
2025-05-16 00:10:12 +0100

    upload, preview and confirmed function done

--- cbt-app/app/Http/Controllers/QuestionImportController.php
+++ cbt-app/app/Http/Controllers/QuestionImportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use App\Models\questions;
 use Illuminate\Http\Request;
 use App\Models\QuestionsImport;
@@ -14,69 +15,98 @@ class QuestionImportController extends Controller
         return view('questions.upload');
     }
 
-    public function import(Request $request){
+    public function preview(Request $request){
         $request->validate([
            'file'=> 'require|mimes:xlsx,csv',
         ]);
-         Excel::import(new QuestionsImport, $request->file('file'));
 
-          return redirect()->back()->with('sucess', 'Questions Imported Successfully');
+        $path = $request->file('file')->getRealPath();
+        $data =Excel::toArray([], $request->file('file'))[0];
+
+        if(count($data) < 2){
+            return redirect()->back()->withErrors(['file' => 'The uploaded file is empty or invalid']);
+        }
+        $hearders = array_map('strtolower', $data[0]);
+        $requiredHeaders = [
+            'subject_id','exam_type','year','question','option_a','option_b','option_c','option_d','option_e','correct_answer'
+        ];
+        if(array_diff($requiredHeaders, $hearders)){
+            return redirect()->back()->withErrors(['file' => ' invalid file format. Ensure correct columns haeders.']); 
+        }
+        $questions = [];
+        foreach (array_slice($data, 1) as  $row) {
+            $questions[] = array_combine($hearders, $row);
+        }
+        return view('questions.preview', compact('questions'));
+
+    }
+    
+    // IMPORT COMFIRMED QUESTIONS
+
+    public function importConfirmed(Request $request)
+    {
+        foreach ($request->questions as $question) {
+            $subject = Subject::where('name', $question['subject'])->first();
+            if(!$subject){
+                continue;
+            }
+            Questions::create([
+'subject_id'=>$subject->id,
+'year'=> $question['year'],
+'exam_type'=> $question['exam_type'],
+'question'=>$question['question'],
+'option_a'=>$question['option_a'],
+'option_b'=>$question['option_b'],
+'option_c'=>$question['option_c'],
+'option_d'=>$question['option_d'],
+'option_e'=>$question['option_e'],
+'correct_answer'=>$question['correct_answer'],
+
+            ]);
+        }
+        return redirect()->route('questions.upload')->with('success', 'Questions Uploaded Successfully');
     }
     
     
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+    // public function index()
+    // {
+    
+    // }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    
+    // public function create()
+    // {
+        
+    // }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    //     public function store(Request $request)
+    // {
+        
+    // }
 
     /**
      * Display the specified resource.
      */
-    public function show(questions $questions)
-    {
-        //
-    }
+    // public function show(questions $questions)
+    // {
+    
+    // }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(questions $questions)
-    {
-        //
-    }
+   
+    // public function edit(questions $questions)
+    // {
+        
+    // }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, questions $questions)
-    {
-        //
-    }
+    
+    // public function update(Request $request, questions $questions)
+    // {
+        
+    // }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(questions $questions)
-    {
-        //
-    }
+   
+    // public function destroy(questions $questions)
+    // {
+        
+    // }
 }
