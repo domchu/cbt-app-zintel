44523b6f8799230dd5d322f668b2f9f02b3d14a9
Nkiru Ujam <ujamcompass@gmail.com>
2025-06-29 20:36:56 +0100

    Intial commit of the homepage design

---
+++ cbt-app/resources/views/components/students-section.blade.php
@@ -0,0 +1,22 @@
+<div class="w-full h-2/5 py-24 bg-[#32064a]">
+    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+     <div class="text-center">
+        <h1 class="text-orange-500 font-bold text-3xl">Students love using QwikTest</h1>
+        <p class="text-white">25,000+ Happy students</p>
+     </div>
+
+     <div class="flex justify-center text-white py-6">
+        <div class="mr-12">
+            <h1 class="font-bold text-4xl">9578+</h1>
+            <p class="text-sm text-center"> Questions Practiced </p>
+        </div>
+        <div class="mr-12">
+            <h1 class="font-bold text-4xl">1354+</h1>
+            <p class="text-sm text-center"> Tests Taken </p>
+        </div>
+        <div class="mr-12">
+            <h1 class="font-bold text-4xl">1758+</h1>
+            <p class="text-sm text-center"> Videos Viewed </p>
+        </div>
+     </div>
+</div>
