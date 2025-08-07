9fa737b8571f178450d4a78401dd676a79d7dc82
Mr.Dominic <ochu.dominic.onah@gmail.com>
2025-08-07 19:59:51 +0100

    clean web route

--- cbt-app/routes/web.php
+++ cbt-app/routes/web.php
@@ -19,25 +19,20 @@ Route::get('/admin/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified', 'rolemanager:admin'])
     ->name('admin');
 
-Route::get('/super-admin/dashboard', function () {
-    return view('admin.super-dashboard');
-})->middleware(['auth', 'verified', 'rolemanager:super-admin'])->name('super-admin');
+    Route::get('/super-admin/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified', 'rolemanager:super-admin'])
+    ->name('super-admin');
 
-
-
-
-
-// Route::get('/dashboard', function () {
-//     return view('admin/dashboard');
-// })->middleware(['auth', 'verified','rolemanager:dashboard'])->name('dashboard');
-
-// Route::get('/admin/dashboard', function () {
-//     return view('admin/admin-dashboard');
-// })->middleware(['auth', 'verified','rolemanager:admin'])->name('admin');
 
 // Route::get('/super-admin/dashboard', function () {
-//     return view('admin/super-dashboard');
-// })->middleware(['auth', 'verified','rolemanager:super-admin'])->name('super-admin');
+//     return view('admin.super-dashboard');
+// })->middleware(['auth', 'verified', 'rolemanager:super-admin'])->name('super-admin');
+
+
+
+
+
+
 
 
 
@@ -85,3 +80,9 @@ Route::get('/faq', function () {
 require __DIR__.'/auth.php';
 
 require __DIR__.'/admin-auth.php';
+
+
+// CHATGPT
+// Browserslist: browsers data (caniuse-lite) is 6 months old. Please run:
+//   npx update-browserslist-db@latest
+//   Why you should do it regularly: https://github.com/browserslist/update-db#readme
