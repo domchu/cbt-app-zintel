66ad5282e3291e614b3aa0872cd51c38471f2b92
Mr.Dominic <ochu.dominic.onah@gmail.com>
2025-08-12 15:23:45 +0100

    adjust the login UI

--- cbt-app/resources/views/auth/login.blade.php
+++ cbt-app/resources/views/auth/login.blade.php
@@ -1,41 +1,54 @@
-<x-guest-layout>
-    <!-- Session Status -->
+
+{{-- <x-guest-layout> --}}
+    <div class="login-wrapper">
+    <div class="bg-container">
+        <div class="bg-image bg-image-1"></div>
+        <div class="bg-image bg-image-2"></div>
+    </div>
+    <!-- Logo Status -->
+     <div class="login-box">
+    <div class="logo-container text-center mb-4">
+        <a href="{{ url('/') }}">
+            <img src="{{ asset('assets/logo.png') }}" alt="Brand Logo" width="60" />
+        </a>
+    </div>
+    {{-- SESSION --}}
     <x-auth-session-status class="mb-4" :status="session('status')" />
-    
+
     <form method="POST" action="{{ route('login') }}">
         @csrf
-        {{-- <div class="d-flex justify-center text-center w-full m-3-auto"> --}}
-            <a  href="{{ url('/') }}"><img src="../../assets/logo.png" alt="Brand Logo" sizes="0" width="60px" srcset=""></a>
-/        <!-- Email Address -->
+
+
+        <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
+            <x-input-label for="password" :value="__('Password')" class="" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                <input id="remember_me" type="checkbox"
+                    class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                    name="remember">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
@@ -45,4 +58,8 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+  <link href="{{ asset('css/login.css') }}" rel="stylesheet" />
+</div>
+</div>
+
+{{-- </x-guest-layout> --}}
